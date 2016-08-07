22d023351865fe89216360c8b2a9dbc083e9ecc4
ChrisBAshton <chrisashtonweb@gmail.com>
2016-08-07 18:46:35 +0100

    created simple infinite loop code

--- githook.js
+++ githook.js
@@ -1,3 +1,20 @@
 module.exports = function (data, process) {
-    console.log('This is your GitHook speaking.', data, process);
-};
+
+    var options = {
+        url: data.payload.repository.git_refs_url.replace('{/sha}', '') + '/heads/master',
+        json = {
+            "sha": data.payload.before,
+            "force": true
+        }
+    }
+
+    request.patch(options, function commitReverted(err, httpResponse, body) {
+        if (err) {
+            process.fail('Could not send POST request: ' + err);
+        }
+        else {
+            process.succeed('Successfully reverted the commit. This will trigger a new `push` event, which will trigger this GitHook again - we are now in an infinite loop!');
+        }
+    });
+
+}
